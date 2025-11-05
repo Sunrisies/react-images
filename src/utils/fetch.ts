aba30afd086bf559f5b0c48558143b467eec9a6c
朱重迁 <z32664206862022@163.com>
2025-11-05 17:33:03 +0800

    refactor(auth): 更新登录API接口和参数命名规范

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,126 +1,145 @@
-import { IPagination } from "@/types"
-import { toast } from "sonner"
+import { IPagination } from "@/types";
+import { toast } from "sonner";
 
 export type GetRequestType<T> = {
-  code: number
-  message: string
+  code: number;
+  message: string;
   data: {
-    data: T,
-    pagination?: IPagination
-  }
-}
+    data: T;
+    pagination?: IPagination;
+  };
+};
 export type PostRequestType<T> = {
-  code: number
-  message: string
-  data: T,
-}
-export type DataOnly<T> = Pick<GetRequestType<T>, 'data'>['data'];
+  code: number;
+  message: string;
+  data: T;
+};
+export type DataOnly<T> = Pick<GetRequestType<T>, "data">["data"];
 // 请求配置类型
 type RequestConfig = {
   headers?: Record<string, string>;
   method?: string;
   body?: any;
   redirect?: RequestRedirect;
-}
+};
 // 错误处理函数类型
-type ErrorHandler = (error: any) => void
+type ErrorHandler = (error: any) => void;
 const errorHandler: ErrorHandler = (error) => {
   switch (error.code) {
     case 200:
       // toast.success(error.message || '请求成功')
-      break
+      break;
     case 400:
       if (Array.isArray(error.data)) {
         // 处理验证错误的情况
         const errorMessage = error.data
-          .map((error: { field: string; errors: string[] }) =>
-            `${error.field}: ${error.errors.join(', ')}`
+          .map(
+            (error: { field: string; errors: string[] }) =>
+              `${error.field}: ${error.errors.join(", ")}`
           )
-          .join('; ');
-        toast.error(errorMessage)
+          .join("; ");
+        toast.error(errorMessage);
       } else {
-        toast.error(error.message)
+        toast.error(error.message);
       }
-      break
+      break;
     case 401:
-      toast.error(error.message || '未登录')
+      toast.error(error.message || "未登录");
       requestAnimationFrame(() => {
-        window.location.href = '/auth/login'
-      })
-      break
+        window.location.href = "/auth/login";
+      });
+      break;
     case 403:
-      toast.error(error.message || '没有权限')
-      break
+      toast.error(error.message || "没有权限");
+      break;
     case 404:
-      toast.error(error.message || '请求资源不存在')
-      break
+      toast.error(error.message || "请求资源不存在");
+      break;
     case 422:
-      toast.error(error.message || '请求参数错误')
-      break
+      toast.error(error.message || "请求参数错误");
+      break;
     case 500:
-      toast.error(error.message || '服务器内部错误')
-      break
+      toast.error(error.message || "服务器内部错误");
+      break;
     default:
-      toast.error(error.message || '请求失败')
-      break
+      toast.error(error.message || "请求失败");
+      break;
   }
-}
+};
 // 基础请求方法
-const baseFetch = async <T, R = GetRequestType<T> | PostRequestType<T>>(url: string, config: RequestConfig): Promise<R> => {
-  const token = sessionStorage.getItem('token')
+const baseFetch = async <T, R = GetRequestType<T> | PostRequestType<T>>(
+  url: string,
+  config: RequestConfig
+): Promise<R> => {
+  const token = sessionStorage.getItem("token");
   const defaultHeaders = {
-    'Content-Type': "application/json",
-    Authorization: token ? `Bearer ${token}` : ''
-  }
+    "Content-Type": "application/json",
+    Authorization: token ? `Bearer ${token}` : "",
+  };
   const response = await fetch(url, {
     ...config,
     headers: {
       ...defaultHeaders,
-      ...config.headers
-    }
-  })
-  const data = await response.json()
-  errorHandler(data)
-  return { ...data } as R
-}
-function RequestInterceptor<T, U>(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
-  const originalMethod = descriptor.value as (url: string, data: T, config: any) => Promise<GetRequestType<U>>
+      ...config.headers,
+    },
+  });
+  const data = await response.json();
+  errorHandler(data);
+  return { ...data } as R;
+};
+function RequestInterceptor<T, U>(
+  target: any,
+  propertyKey: string,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value as (
+    url: string,
+    data: T,
+    config: any
+  ) => Promise<GetRequestType<U>>;
 
-  descriptor.value = async function (url: string, data: T): Promise<GetRequestType<U>> {
+  descriptor.value = async function (
+    url: string,
+    data: T
+  ): Promise<GetRequestType<U>> {
     // 请求拦截器逻辑
-    console.log('请求拦截器：', url, data)
-    const token = sessionStorage.getItem('token')
+    console.log("请求拦截器：", url, data);
+    const token = sessionStorage.getItem("token");
     const config = {
       headers: {
-        'Content-Type': 'application/json',
-        Authorization: token ? `Bearer ${token}` : ''
-      }
-    }
+        "Content-Type": "application/json",
+        Authorization: token ? `Bearer ${token}` : "",
+      },
+    };
     // 调用原始方法
-    const response = await originalMethod.apply(this, [url, data, config])
-    console.log(response, 'response')
-    return response
-  }
+    const response = await originalMethod.apply(this, [url, data, config]);
+    console.log(response, "response");
+    return response;
+  };
 
-  return descriptor
+  return descriptor;
 }
 
-function ResponseInterceptor(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
-  const originalMethod = descriptor.value
+function ResponseInterceptor(
+  target: any,
+  propertyKey: string,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
 
   descriptor.value = async function (...args: any[]) {
-    const response = await originalMethod.apply(this, args)
-    return response
-  }
+    const response = await originalMethod.apply(this, args);
+    return response;
+  };
 
-  return descriptor
+  return descriptor;
 }
 class Request {
   constructor(private readonly BaseUrl: string) {
-    this.BaseUrl = BaseUrl
+    this.BaseUrl = BaseUrl;
   }
   private getFullUrl(url: string): string {
-    return this.BaseUrl + url
+    return this.BaseUrl + url;
   }
   /**
    * 发送一个 GET 请求。
@@ -135,16 +154,16 @@ class Request {
    *
    * @throws {Error} - 如果请求失败，抛出错误信息。
    */
-  async get<U>(url: string): Promise<GetRequestType<U>> // 当没有 data 参数时的重载
-  async get<U, T>(url: string, data: T): Promise<GetRequestType<U>> // 当有 data 参数时的重载
+  async get<U>(url: string): Promise<GetRequestType<U>>; // 当没有 data 参数时的重载
+  async get<U, T>(url: string, data: T): Promise<GetRequestType<U>>; // 当有 data 参数时的重载
   async get<U, T>(url: string, data?: T): Promise<GetRequestType<U> | Error> {
-    console.log(this.BaseUrl + url)
+    console.log(this.BaseUrl + url);
     const queryUrl = data
       ? `${url}?${new URLSearchParams(data as any).toString()}`
-      : url
+      : url;
     return baseFetch<U, GetRequestType<U>>(this.getFullUrl(queryUrl), {
-      method: 'GET'
-    })
+      method: "GET",
+    });
   }
   /**
    * 发送一个 POST 请求。
@@ -161,11 +180,15 @@ class Request {
    */
   @RequestInterceptor
   @ResponseInterceptor
-  async post<T, U>(url: string, data: T, config?: any): Promise<PostRequestType<U>> {
+  async post<T, U>(
+    url: string,
+    data: T,
+    config?: any
+  ): Promise<PostRequestType<U>> {
     return baseFetch<U, PostRequestType<U>>(this.getFullUrl(url), {
-      method: 'POST',
-      body: JSON.stringify(data)
-    })
+      method: "POST",
+      body: JSON.stringify(data),
+    });
   }
   /**
    * 发送一个 PUT 请求。
@@ -177,9 +200,9 @@ class Request {
   @ResponseInterceptor
   async put<T, U>(url: string, data: T): Promise<any> {
     return baseFetch<U>(this.getFullUrl(url), {
-      method: 'PUT',
-      body: JSON.stringify(data)
-    })
+      method: "PUT",
+      body: JSON.stringify(data),
+    });
   }
 
   /**
@@ -192,25 +215,24 @@ class Request {
   @ResponseInterceptor
   async delete<T, U>(url: string): Promise<any> {
     return baseFetch<U>(this.getFullUrl(url), {
-      method: 'DELETE'
-    })
+      method: "DELETE",
+    });
   }
-
 
   /** 上传文件 */
 
   async upload<U>(url: string, body: FormData): Promise<any> {
     const response = await fetch(this.BaseUrl + url, {
-      method: 'POST',
+      method: "POST",
       body,
-      redirect: 'follow'
-    })
+      redirect: "follow",
+    });
     if (response.ok) {
-      return await response.json()
+      return await response.json();
     }
-    return Promise.reject(response.statusText)
+    return Promise.reject(response.statusText);
   }
 }
-const BaseUrl = 'https://api.chaoyang1024.top:2345/api'
+const BaseUrl = "https://api.chaoyang1024.top:12345/api";
 // const BaseUrl = 'http://localhost:2345/api'
-export const request = new Request(BaseUrl)
+export const request = new Request(BaseUrl);
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import path from "path"
-import { visualizer } from 'rollup-plugin-visualizer'
-import viteCompression from 'vite-plugin-compression'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import path from "path";
+import { visualizer } from "rollup-plugin-visualizer";
+import viteCompression from "vite-plugin-compression";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [TanStackRouterVite(), react()],
@@ -14,47 +14,48 @@ export default defineConfig({
   },
   server: {
     open: true,
-    host:'0.0.0.0'
+    host: "0.0.0.0",
+    port: 12002,
   },
   css: {
     preprocessorOptions: {
       scss: {
-        api:'modern-compiler'
-      }
-    }
+        api: "modern-compiler",
+      },
+    },
   },
   build: {
-    target: 'es2020',
-    minify: 'terser',
+    target: "es2020",
+    minify: "terser",
     // rollup 配置
     rollupOptions: {
       output: {
-        chunkFileNames: 'js/[name]-[hash].js', // 引入文件名的名称
-        entryFileNames: 'js/[name]-[hash].js', // 包的入口文件名称
-        assetFileNames: '[ext]/[name]-[hash].[ext]' // 资源文件像 字体，图片等
+        chunkFileNames: "js/[name]-[hash].js", // 引入文件名的名称
+        entryFileNames: "js/[name]-[hash].js", // 包的入口文件名称
+        assetFileNames: "[ext]/[name]-[hash].[ext]", // 资源文件像 字体，图片等
       },
       plugins: [
         visualizer({
           open: true, // 直接在浏览器中打开分析报告
-          filename: 'stats.html', // 输出文件的名称
+          filename: "stats.html", // 输出文件的名称
           gzipSize: true, // 显示gzip后的大小
-          brotliSize: true // 显示brotli压缩后的大小
+          brotliSize: true, // 显示brotli压缩后的大小
         }),
         viteCompression({
           verbose: true, // 是否在控制台中输出压缩结果
           disable: false,
           threshold: 10240, // 如果体积大于阈值，将被压缩，单位为b，体积过小时请不要压缩，以免适得其反
-          algorithm: 'gzip', // 压缩算法，可选['gzip'，' brotliccompress '，'deflate '，'deflateRaw']
-          ext: '.gz',
-          deleteOriginFile: false // 源文件压缩后是否删除
-        })
-      ]
+          algorithm: "gzip", // 压缩算法，可选['gzip'，' brotliccompress '，'deflate '，'deflateRaw']
+          ext: ".gz",
+          deleteOriginFile: false, // 源文件压缩后是否删除
+        }),
+      ],
     },
     terserOptions: {
       compress: {
         drop_console: true,
-        drop_debugger: true
-      }
-    }
+        drop_debugger: true,
+      },
+    },
   },
-})
+});
